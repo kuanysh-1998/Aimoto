0de33ecf4757de27165045bcefda6798906fc723
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2022-12-15 13:18:52 +0300

    made a beautiful login page for admin panel

--- src/components/AdminPanel/AuthProvider.jsx
+++ src/components/AdminPanel/AuthProvider.jsx
@@ -1,24 +1,31 @@
-
 export const authProvider = {
-    login: ({ username }) => {
-      localStorage.setItem("username", username);
-      return Promise.resolve();
+    login: ({ username, password }) => {
+        if (username !== 'engineers@knopka24.ru' || password !== 'engineersaimoto') {
+            return Promise.reject();
+        }
+        localStorage.setItem('username', username);
+        return Promise.resolve();
     },
     logout: () => {
-      localStorage.removeItem("username");
-      return Promise.resolve();
+        localStorage.removeItem('username');
+        return Promise.resolve();
     },
-    checkError: ({ status }) => {
-      if (status === 401 || status === 403) {
-        localStorage.removeItem("username");
-        return Promise.reject();
-      }
-      return Promise.resolve();
+        checkAuth: () =>
+        localStorage.getItem('username') ? Promise.resolve() : Promise.reject(),
+    checkError:  (error) => {
+        const status = error.status;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        // other error code (404, 500, etc): no need to log out
+        return Promise.resolve();
     },
-    checkAuth: () => {
-      return localStorage.getItem("username")
-        ? Promise.resolve()
-        : Promise.reject();
-    },
-    getPermissions: () => Promise.resolve(),
-  };
+    getIdentity: () =>
+        Promise.resolve({
+            id: 'user',
+            fullName: 'Help Desk',
+        }),
+    getPermissions: () => Promise.resolve(''),
+};
+
