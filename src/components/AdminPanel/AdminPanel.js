17224e810dadd6f1b93a4ea490b7891cab4ed6b0
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2022-12-15 14:51:47 +0300

    resize input

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -10,7 +10,7 @@ const dataProvider = jsonServerProvider(
 function AdminPanel() {
   return (
     <div className="admin">
-      
+
         <Admin dataProvider={dataProvider} authProvider={authProvider}>
           <Resource
             name="proshivka"
@@ -19,6 +19,7 @@ function AdminPanel() {
             create={PostCreate}
           />
         </Admin>
+
     </div>
   );
 }
