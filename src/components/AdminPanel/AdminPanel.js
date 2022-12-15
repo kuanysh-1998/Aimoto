8ae6cce580ecce22a3c5f6940cecd62fef30a2d6
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2022-12-15 11:43:44 +0300

    added a login page

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -2,6 +2,7 @@ import "./admin.scss";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { PostList, PostEdit, PostCreate } from "./Proshivka";
+import { authProvider } from "./AuthProvider";
 
 const dataProvider = jsonServerProvider(
   "https://aimoto-server-production.up.railway.app"
@@ -9,14 +10,15 @@ const dataProvider = jsonServerProvider(
 function AdminPanel() {
   return (
     <div className="admin">
-      <Admin dataProvider={dataProvider} >
-        <Resource
-          name="proshivka"
-          list={PostList}
-          edit={PostEdit}
-          create={PostCreate}
-        />
-      </Admin>
+      
+        <Admin dataProvider={dataProvider} authProvider={authProvider}>
+          <Resource
+            name="proshivka"
+            list={PostList}
+            edit={PostEdit}
+            create={PostCreate}
+          />
+        </Admin>
     </div>
   );
 }
--- src/components/AdminPanel/Proshivka.js
+++ src/components/AdminPanel/Proshivka.js
@@ -1,42 +1,69 @@
-import { List, Datagrid, TextField, Edit, SimpleForm, TextInput, Create} from 'react-admin';
-import BookIcon from '@mui/icons-material/Book';
+import {
+  List,
+  Datagrid,
+  TextField,
+  Edit,
+  SimpleForm,
+  TextInput,
+  Create,
+  SimpleList,
+  ReferenceInput
+} from "react-admin";
+import BookIcon from "@mui/icons-material/Book";
+import { useMediaQuery } from "@mui/material";
 export const PostIcon = BookIcon;
 
-export const PostList = (props) => (
-    <List {...props}>
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="id" label="User" reference="imei" />,
+];
+
+export const PostList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  return (
+    <List filters={postFilters} >
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.model}
+          secondaryText={(record) => record.imei}
+          thirdlyText={(record) => record.file}
+        />
+      ) : (
         <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="imei" />
-            <TextField source="model" />
-            <TextField source="file" />
-            <TextField source="program" />
-            <TextField source="Comment" />
+          <TextField source="id" />
+          <TextField source="imei" />
+          <TextField source="model" />
+          <TextField source="file" />
+          <TextField source="program" />
+          <TextField source="Comment" />
         </Datagrid>
+      )}
     </List>
+  );
+};
+
+export const PostEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="imei" />
+      <TextInput source="model" />
+      <TextInput source="file" />
+      <TextInput source="program" />
+      <TextInput source="Comment" />
+    </SimpleForm>
+  </Edit>
 );
 
-export const PostEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput disabled source="id" />
-            <TextInput source="imei" />
-            <TextInput source="model" />
-            <TextInput source="file" />
-            <TextInput source="program" />
-            <TextInput source="Comment" />
-        </SimpleForm>
-    </Edit>
-);
-
-export const PostCreate = props => (
-    <Create {...props}>
-        <SimpleForm>
-        <TextInput source="id" />
-            <TextInput source="imei" />
-            <TextInput source="model" />
-            <TextInput source="file" />
-            <TextInput source="program" />
-            <TextInput source="Comment" />
-        </SimpleForm>
-    </Create>
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="id" />
+      <TextInput source="imei" />
+      <TextInput source="model" />
+      <TextInput source="file" />
+      <TextInput source="program" />
+      <TextInput source="Comment" />
+    </SimpleForm>
+  </Create>
 );
